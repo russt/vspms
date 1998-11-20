88c2d48ac50c7a681f49962889fe6f0a04b9fb9b
Russ Tremain <russt@releasetools.org>
1998-11-20 10:30:39 -0800

    added %include, -lspj.

--- src/c/bin/wherepj.c
+++ src/c/bin/wherepj.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define	DEBUG 0
+
 #define		streq(s1,s2)	(strcmp(s1,s2) == 0)
 
 #define PJ_MAP_VAR "MYPROJECTS"
 #define PROJECT_VAR "PROJECT"
 
+extern int errno;
 extern char *projname();
 extern char *projdir();
 extern char *getenv();
+
+char prog[BUFSIZ];
 
 main(argc,argv,envp)
 /*
@@ -19,9 +24,10 @@ int argc;
 char *argv[];
 char *envp[];
 {
-	char buf[BUFSIZ],*p, proj_map[BUFSIZ];
-	FILE *fp;
 	int argn = 1, subpj = 0;
+	char *p, proj_map[BUFSIZ], dirstr[BUFSIZ];
+
+	strcpy(prog,argv[0]);
 
 	/*
 	** first, check to see if project management environment
@@ -32,22 +38,12 @@ char *envp[];
 
 	if (p == NULL)
 	{
-		fprintf(stderr,"%s:  %s is undefined\n",argv[0],PJ_MAP_VAR);
+		fprintf(stderr,"%s:  %s is undefined\n",prog,PJ_MAP_VAR);
 		fprintf(stdout,"NULL\n");
-		exit(-1);
+		exit(1);
 	}
 	else
 		strcpy(proj_map,p);
-
-	/*
-	** is the project list file accessible?
-	*/
-	if ((fp = fopen(proj_map,"r")) == NULL)
-	{
-		fprintf(stderr,"%s:  can't open %s\n",argv[0],proj_map);
-		fprintf(stdout,"NULL\n");
-		exit(-1);
-	}
 
 	/*
 	** default is to echo nothing, so chpj will re-establish the
@@ -57,10 +53,23 @@ char *envp[];
 	if (argn >= argc)
 		exit(0);
 
-	/* if sub-project option... */
-	if (streq(argv[argn],"-s")) {
-		subpj = 1;
-		++argn;
+	/*
+	** PROCESS FLAGS:
+	*/
+
+
+	while (argn < argc && argv[argn][0] == '-') {
+		if (streq(argv[argn],"-s")) {
+			subpj = 1;
+			++argn;
+		} else if (streq(argv[argn],"-lspj")) {
+			lspj(proj_map);
+			++argn;
+			exit(0);
+		} else {
+			fprintf(stderr,"%s:  '%s' is not a valid option.\n",prog,argv[argn]);
+			exit(1);
+		}
 	}
 
 	if (argn >= argc)
@@ -83,7 +92,7 @@ char *envp[];
 
 		if (p == NULL)
 		{
-			fprintf(stderr,"%s:  %s is undefined\n",argv[0],
+			fprintf(stderr,"%s:  %s is undefined\n",prog,
 				PROJECT_VAR);
 			fprintf(stdout,"NULL\n");
 			exit(-1);
@@ -96,16 +105,105 @@ char *envp[];
 	/*
 	** Otherwise, search the project file:
 	*/
+	if (searchpjfile(&dirstr[0], argv[argn], proj_map)) {
+		fprintf(stdout,"%s\n",dirstr);
+		exit(0);
+	} else {
+		fprintf(stderr,"%s:  no such project, %s\n",prog, argv[argn]);
+		fprintf(stdout,"NULL\n",dirstr);
+	}
+
+	exit(1);
+}
+
+int
+lspj(fn)
+/*
+** list the project index files, and interprolate the include files.
+*/
+char fn[];
+{
+	char buf[BUFSIZ], proj[BUFSIZ];
+	FILE *fp;
+
+	/*
+	** is the project list file accessible?
+	*/
+	if ((fp = fopen(fn,"r")) == NULL)
+	{
+		fprintf(stderr, "%s:  can't open %s\n",prog,fn);
+		perror("wherepj");
+		fprintf(stdout,"NULL\n");
+		exit(1);
+	}
+
+	/*
+	** Otherwise, search the project file:
+	*/
 	while (fgets(buf,BUFSIZ,fp) != NULL) {
-		if (streq(argv[argn], projname(buf))) {
-			fputs(projdir(buf),stdout);
-			exit(0);
+		strcpy(proj,projname(buf));
+		if (streq(proj, "%include")) {
+			/* PROCESS INCLUDE file */
+			fprintf(stdout, "%s", buf);
+			lspj(projdir(buf));
+			fprintf(stdout, "<END INCLUDE>\n");
+		} else {
+			fputs(buf,stdout);
 		}
 	}
 
-	fprintf(stderr,"%s:  no such project, %s\n",argv[0], argv[argn]);
-	fprintf(stdout,"NULL\n");
-	exit(-1);
+	fclose(fp);
+	return(0);
+}
+int
+searchpjfile(dirstr, look4, fn)
+/*
+** look in <fn> for <look4>.  if found, return true with result in <dirstr>.
+** if not found, return false with "NULL" in <dirstr>.
+*/
+char *dirstr, look4[], fn[];
+{
+	char buf[BUFSIZ], buf2[BUFSIZ], proj[BUFSIZ];
+	FILE *fp;
+
+	strcpy(dirstr,"NULL");
+
+#if DEBUG
+fprintf(stderr, "looking for '%s' in file '%s'\n", look4, fn);
+#endif DEBUG
+
+	/*
+	** is the project list file accessible?
+	*/
+	if ((fp = fopen(fn,"r")) == NULL)
+	{
+		fprintf(stderr, "%s:  can't open %s\n",prog,fn);
+		perror("wherepj");
+		fprintf(stdout,"NULL\n");
+		exit(1);
+	}
+
+	/*
+	** Otherwise, search the project file:
+	*/
+	while (fgets(buf,BUFSIZ,fp) != NULL) {
+		strcpy(proj,projname(buf));
+		if (streq(proj, "%include")) {
+			/* PROCESS INCLUDE file */
+			if (searchpjfile(&buf2[0], look4, projdir(buf))) {
+				strcpy(dirstr, buf2);
+				fclose(fp);
+				return(1);		/* found in include file */
+			}
+		} else if (streq(look4, proj)) {
+			strcpy(dirstr, projdir(buf));
+			fclose(fp);
+			return(1);
+		}
+	}
+
+	fclose(fp);
+	return(0);
 }
 
 char *
@@ -127,6 +225,11 @@ char *buf;
 	}
 
 	retval[i] = '\0';
+
+#if DEBUG > 1
+fprintf(stderr,"projname='%s'\n",retval);
+#endif DEBUG
+
 	return(retval);
 }
 
@@ -156,6 +259,15 @@ char *buf;
 		++buf;
 	}
 
-	retval[i] = '\0';
+	if (i <= 0) {
+		retval[i] = '\0';
+	} else {
+		retval[i-1] = '\0';
+	}
+
+#if DEBUG > 1
+fprintf(stderr,"projdir='%s'\n",retval);
+#endif DEBUG
+
 	return(retval);
 }
